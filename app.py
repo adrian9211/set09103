a4045b9701f50a6ac05a91446196d4a0edc04eb9
adrian9211 <adrian.nykiel@gmail.com>
2023-09-21 13:59:45 +0100

    app.py modify

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!!!!!'
+    return 'Hello'
+
+
+@app.route('/bye/')
+def bye():
+    return 'Bye!'
+
+
+@app.route('/hello/')
+def hello():
+    return 'Hello Napier!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
