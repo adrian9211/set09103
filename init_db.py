7e90cb360e038b307d4243471a2408eae2ab93d5
adrian9211 <adrian.nykiel@gmail.com>
2023-11-14 21:40:51 +0000

    basic project structure

--- init_db.py
+++ init_db.py
@@ -12,6 +12,7 @@ cur.execute("INSERT INTO notes (content) VALUES (?)", ('# The First Note',))
 cur.execute("INSERT INTO notes (content) VALUES (?)", ('# Second Note',))
 cur.execute("INSERT INTO notes (content) VALUES (?)", ('# Third Note',))
 cur.execute("INSERT INTO notes (content) VALUES (?)", ('# Fourth Note',))
+cur.execute("INSERT INTO User (email) VALUES (?)", ('adrian9211@gmail.com',))
 
 
 connection.commit()
